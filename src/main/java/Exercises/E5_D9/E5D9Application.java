46a49a6b186c7b383bbf43f85ca665253f0d2401
Andrea Cavallo <astarore@gmail.com>
2023-11-09 16:24:12 +0100

    Initial commit

---
+++ src/main/java/Exercises/E5_D9/E5D9Application.java
@@ -0,0 +1,13 @@
+package Exercises.E5_D9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E5D9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E5D9Application.class, args);
+	}
+
+}
---
+++ src/test/java/Exercises/E5_D9/E5D9ApplicationTests.java
@@ -0,0 +1,13 @@
+package Exercises.E5_D9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E5D9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
